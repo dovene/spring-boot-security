984481ce35d7ae6749aa166c8dda9d0a873bf753
DOVENE AGOGUE <adovene@gmail.com>
2021-08-17 14:18:54 +0200

    add oauth2 security layer

--- src/main/java/com/dov/securityapp/controller/LoginController.java
+++ src/main/java/com/dov/securityapp/controller/LoginController.java
@@ -1,25 +1,82 @@
 package com.dov.securityapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
-public class LoginController
+public class LoginController {
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
 
-{
     @RolesAllowed("USER")
-    @RequestMapping("/*")
-    public String getUser()
-    {
+    @RequestMapping("/**")
+    public String getUser() {
         return "Welcome User";
     }
 
-    @RolesAllowed({"USER","ADMIN"})
+    @RolesAllowed({"USER", "ADMIN"})
     @RequestMapping("/admin")
-    public String getAdmin()
-    {
+    public String getAdmin() {
         return "Welcome Admin";
+    }
+
+
+    @RequestMapping("/*")
+    public String getUserInfo(Principal user) {
+        StringBuffer userInfo= new StringBuffer();
+        if(user instanceof UsernamePasswordAuthenticationToken){
+            userInfo.append(getUsernamePasswordLoginInfo(user));
+        }
+        else if(user instanceof OAuth2AuthenticationToken){
+            userInfo.append(getOauth2LoginInfo(user));
+        }
+        return userInfo.toString();
+    }
+
+
+    private StringBuffer getUsernamePasswordLoginInfo(Principal user) {
+        StringBuffer usernameInfo = new StringBuffer();
+
+        UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken) user);
+        if (token.isAuthenticated()) {
+            User u = (User) token.getPrincipal();
+            usernameInfo.append("Welcome, " + u.getUsername());
+        } else {
+            usernameInfo.append("NA");
+        }
+        return usernameInfo;
+    }
+
+    private StringBuffer getOauth2LoginInfo(Principal user){
+
+        StringBuffer protectedInfo = new StringBuffer();
+
+        OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
+        OAuth2AuthorizedClient authClient = this.authorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+        if(authToken.isAuthenticated()){
+
+            Map<String,Object> userAttributes = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
+
+            String userToken = authClient.getAccessToken().getTokenValue();
+            protectedInfo.append("Welcome, " + userAttributes.get("name")+"<br><br>");
+            protectedInfo.append("e-mail: " + userAttributes.get("email")+"<br><br>");
+            protectedInfo.append("Access Token: " + userToken+"<br><br>");
+        }
+        else{
+            protectedInfo.append("NA");
+        }
+        return protectedInfo;
     }
 }
