458a458c70f5108d3ede7c91362620ba1213323a
DOVENE AGOGUE <adovene@gmail.com>
2021-08-17 14:54:10 +0200

    added unit tests

--- src/test/java/com/dov/securityapp/SecurityAppApplicationTests.java
+++ src/test/java/com/dov/securityapp/SecurityAppApplicationTests.java
@@ -1,13 +1,24 @@
 package com.dov.securityapp;
 
+import com.dov.securityapp.controller.LoginController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SecurityAppApplicationTests {
 
+	@Autowired
+	private LoginController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads()throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
